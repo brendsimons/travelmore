6253823a1566fa4c6901bda3fcbf22a0694880bf
Jari Mathe <jari.mathe@hotmail.com>
2018-10-26 11:33:18 +0200

    werk aan login

--- src/main/java/be/thomasmore/travelmore/controller/UserController.java
+++ src/main/java/be/thomasmore/travelmore/controller/UserController.java
@@ -8,11 +8,16 @@ import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
 import java.util.List;
 
+import java.io.IOException;
+import javax.faces.context.FacesContext;
+import javax.faces.application.FacesMessage;
+
 @ManagedBean
 @ViewScoped
 public class UserController {
 
     private User newUser = new User();
+    private User gebruikteUser = new User();
 
     @Inject
     private UserService userService;
@@ -29,9 +34,42 @@ public class UserController {
         return this.userService.findAllUsers();
     }
 
-    public String submit(){
+    public void submit(){
+        this.userService.insert(newUser);
+    }
+
+    public String submitRegister(){
         this.userService.insert(newUser);
 
         return "registerBedankt";
     }
+
+    public User getGebruikteUser(){
+        return gebruikteUser;
+    }
+
+    public String login(){
+        this.userService.compareLogin(gebruikteUser);
+
+        return "registerBedankt";
+    }
+
+//    public void login(){
+//        FacesContext context = FacesContext.getCurrentInstance();
+//
+//        if(gebruikteUser.getEmail().equals("email") && gebruikteUser.getPassword().equals("password")){
+//            context.getExternalContext().getSessionMap().put("email", gebruikteUser.getEmail());
+//            try {
+//                context.getExternalContext().redirect("registerBedankt.xhtml");
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        else  {
+//            //Send an error message on Login Failure
+//            context.addMessage(null, new FacesMessage("Authentication Failed. Check username or password."));
+//
+//        }
+//    }
+
 }
